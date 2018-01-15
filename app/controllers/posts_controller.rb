213a72ba3258248f039c11ad526f21aa22c2c842
YUSUKE KUBO <sd31.mawhdwhb@gmail.com>
2018-01-16 00:50:05 +0900

    add CRUD of Model Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,38 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: [:show, :edit, :update, :destory]
+
   def index
     @posts = Post.all
+  end
 
-    @test = Post.new
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html {redirect_to @post, notice: '登録処理が正常に終了しました'}
+        # format.json {render :show, status: :created, location: @post}
+      else
+        format.html {render :new}
+        # format.json {render json: @book.errors, status: unprocessable_entity}
+      end
+    end
+  end
+
+private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'posts/index'
+  # get 'posts/index'
   get '/', to: 'home#index'
   resources :posts
 
