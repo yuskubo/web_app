8f324b9bbf1eff60e1afc482fa34bc7a3a9ef9c5
YUSUKE KUBO <26254464+yuskubo@users.noreply.github.com>
2018-02-11 00:46:14 +0900

    学習項目ボタンクリック時の処理を追加

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,11 +1,34 @@
-// document.getElementById("text-button").onclick = function() {
-//   document.getElementById("text").innerHTML = "クリックされた！";
-// };
 
-document.getElementById("ror-button").onclick = function() {
-  var disp_status_ror = document.getElementById("ror-text");
-  var disp_status_css = document.getElementById("css-text");
-  var disp_status_javascript = document.getElementById("javascript-text");
+function ror_btn(){
+  var btn = "ror-btn";
+  change_display(btn);
+}
 
-  disp_status_ror.style.display = "block";
-};
+function css_btn(){
+  var btn = "css-btn";
+  change_display(btn);
+}
+
+function jav_btn(){
+  var btn = "jav-btn";
+  change_display(btn);
+}
+
+function change_display(btn) {
+  var ror_detail = document.getElementById("ror-text");
+  var css_detail = document.getElementById("css-text");
+  var jav_detail = document.getElementById("jav-text");
+
+  ror_detail.style.display = "none";
+  css_detail.style.display = "none";
+  jav_detail.style.display = "none";
+
+  if (btn == "ror-btn") {
+    ror_detail.style.display = "block";
+  } else if( btn == "css-btn") {
+    css_detail.style.display = "block";
+  } else if(btn == "jav-btn") {
+    jav_detail.style.display = "block";
+  }
+
+}
