9ee2d8f4e55130f3bf29e77f1d1a457d31085111
YUSUKE KUBO <sd31.mawhdwhb@gmail.com>
2018-03-11 20:37:48 +0900

    フロントエンドの改修

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -32,3 +32,30 @@ function change_display(btn) {
   }
 
 }
+
+
+$(function(){
+
+// モーダルウィンドウが開くときの処理
+$(".modalOpen").click(function(){
+
+    var navClass = $(this).attr("class"),
+        href = $(this).attr("href");
+
+        $(href).fadeIn();
+    $(this).addClass("open");
+    return false;
+});
+
+// モーダルウィンドウが閉じるときの処理
+$(".modalClose").click(function(){
+    $(this).parents(".modal").fadeOut();
+    $(".modalOpen").removeClass("open");
+    return false;
+});
+
+});
+
+$(document).ready(function() {
+ $("p").css("color", "blue");
+});
