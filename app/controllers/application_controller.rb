65be1a1aa8b8f728548561717f893e9880fdc8ad
YUSUKE KUBO <26254464+yuskubo@users.noreply.github.com>
2018-03-22 00:24:27 +0900

    redisの導入と認証処理作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
+  before_action :check_logined
+
+  private
+    def check_logined
+      if session[:user_name] then
+        begin
+          @user = User.find(session[:user_name])
+        rescue ActiveRecord::RecordNotFound
+          reset_session
+        end
+      end
+
+    unless @user
+      flash[:referer] = request.fullpath
+      redirect_to controller: :sessions, action: :index
+    end
+    end
 end
